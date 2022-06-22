54cc05cc1b85382bd54d896ebf8561beff8bcc38
lexggwp <alexeyfilipp@gmail.com>
2022-06-23 00:16:14 +0300

    added: arrays for props Dialogs.tsx

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -12,7 +12,9 @@ type DialogPropsType = {
 
 type MessagePropsType = {
     message: string | number;
+    id: number;
 }
+
 
 const DialogItem = (props: DialogPropsType) => {
     return (
@@ -29,22 +31,34 @@ const Message = (props: MessagePropsType) => {
 }
 
 const Dialogs = () => {
+
+    let dialogsData: Array<DialogPropsType> = [{id: 1, name: 'Alexey'},
+        {id: 2, name: 'Sasha'},
+        {id: 3, name: 'Dima'},
+        {id: 4, name: 'Petr'},
+        {id: 5, name: 'Sasha'}];
+
+    let messageData: Array<MessagePropsType> = [{id: 1, message: 'How are you?!'},
+        {id: 2, message: 'Hi there!'},
+        {id: 3, message: 'Glad to see you.'},
+        {id: 4, message: 'Petr'},
+        {id: 5, message: 'Nice nice nice'}];
+
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogs_items}>
-               <DialogItem name={'Alexey'} id={1} />
-               <DialogItem name={'Sasha'} id={2} />
-               <DialogItem name={'Dima'} id={3} />
-               <DialogItem name={'Petr'} id={4} />
-               <DialogItem name={'Nikolai'} id={5} />
-               <DialogItem name={'Oleg'} id={6} />
-
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
             </div>
             <div className={s.messages}>
-                <Message message={'Hi there!'}/>
-                <Message message={'How are you?!'}/>
-                <Message message={'Glad to see you.'}/>
-                <Message message={'Nice nice nice'}/>
+                <Message message={messageData[0].message} id={messageData[0].id}/>
+                <Message message={messageData[1].message} id={messageData[1].id}/>
+                <Message message={messageData[2].message} id={messageData[2].id}/>
+                <Message message={messageData[3].message} id={messageData[4].id}/>
 
 
             </div>
