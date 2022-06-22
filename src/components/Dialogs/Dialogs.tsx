60c55b269aefed23f932936248d8d0ed7e66bf97
lexggwp <alexeyfilipp@gmail.com>
2022-06-22 22:47:41 +0300

    added: NavLink's to Dialogs.tsx

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
-        <div>
-            
+        <div className={s.dialogs}>
+            <div className={s.dialogs_items}>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/1">Andrey</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/2">Katya</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">Stepan</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/4">Sasha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/5">Vlad</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you</div>
+                <div className={s.message}>kek kek ke ke</div>
+                <div className={s.message}>Ti kto?</div>
+            </div>
         </div>
     );
 };
